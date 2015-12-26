fc0ba9e46216e08ff7bac22057d95cd39f76be22
Andy Fragen <andy@thefragens.com>
2015-12-26 11:39:54 -0800

    exit early for heartbeat and no settings

--- src/Local_Development_Upgrade_Warning/Init.php
+++ src/Local_Development_Upgrade_Warning/Init.php
@@ -20,9 +20,16 @@ class Init {
 	 * Init constructor.
 	 */
 	public function __construct() {
-		$config = get_site_option( 'local_development_upgrade_warning' );
 
+		$config = get_site_option( 'local_development_upgrade_warning' );
 		new Settings();
+
+		/*
+		 * Skip on hearbeat or if no saved settings.
+		 */
+		if ( ( isset( $_POST['action'] ) && 'heartbeat' === $_POST['action'] ) || ! $config ) {
+			return false;
+		}
 		new Base( $config );
 	}
 }
