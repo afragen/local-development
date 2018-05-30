f67669533b9453e5b5a77f6edbb50b48a33aa46d
Andy Fragen <andy@thefragens.com>
2018-05-30 15:54:33 -0700

    combine saving to single hook

--- src/Local_Development/Settings.php
+++ src/Local_Development/Settings.php
@@ -48,8 +48,7 @@ class Settings {
 	public function load_hooks() {
 		add_action('init', [$this, 'init']);
 		add_action(is_multisite() ? 'network_admin_menu' : 'admin_menu', [$this, 'add_plugin_page']);
-		add_action('network_admin_edit_local-development', [$this, 'update_settings']);
-		add_action('admin_init', [$this, 'update_settings']);
+		add_action(is_multisite() ? 'network_admin_edit_local-development' : 'admin_init', [$this, 'update_settings']);
 		add_action('admin_head-settings_page_local-development', [$this, 'style_settings']);
 
 		add_filter(
