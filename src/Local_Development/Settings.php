64715e5bee17f58366042c770c51f65b72f2ee41
Andy Fragen <andy@thefragens.com>
2019-01-24 09:42:13 -0800

    precise comparison

--- src/Local_Development/Settings.php
+++ src/Local_Development/Settings.php
@@ -156,7 +156,7 @@ class Settings {
 		$current_tab = isset( $_GET['tab'] ) ? $_GET['tab'] : 'local_dev_settings_plugins';
 		echo '<h2 class="nav-tab-wrapper">';
 		foreach ( $this->settings_tabs() as $key => $name ) {
-			$active = ( $current_tab == $key ) ? 'nav-tab-active' : '';
+			$active = ( $current_tab === $key ) ? 'nav-tab-active' : '';
 			echo '<a class="nav-tab ' . $active . '" href="?page=local-development&tab=' . $key . '">' . $name . '</a>';
 		}
 		echo '</h2>';
