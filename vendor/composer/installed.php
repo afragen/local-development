94fb092326f5ad8a0d9c418b0ef93e8e10e0b597
Andy Fragen <andy@thefragens.com>
2023-03-19 09:46:09 -0700

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,43 +1,43 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-develop',
-    'version' => 'dev-develop',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'name' => 'afragen/local-development',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '2805ead45f5860de2b1343f34594fa4b337d87e3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
     ),
-    'reference' => '2928e88ea35bd9309fda60b9c8d54697b8622c49',
-    'name' => 'afragen/local-development',
-  ),
-  'versions' => 
-  array (
-    'afragen/local-development' => 
-    array (
-      'pretty_version' => 'dev-develop',
-      'version' => 'dev-develop',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '2928e88ea35bd9309fda60b9c8d54697b8622c49',
+    'versions' => array(
+        'afragen/local-development' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '2805ead45f5860de2b1343f34594fa4b337d87e3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'afragen/singleton' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '011f7eb2fd66a3c28ee82503fa2f7ae7cd9e4288',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../afragen/singleton',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/wp-config-transformer' => array(
+            'pretty_version' => 'v1.3.1',
+            'version' => '1.3.1.0',
+            'reference' => 'c5b5349b86a3eea6c8a3f401f556f21a717aa80e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/wp-config-transformer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
     ),
-    'afragen/singleton' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => 'be8e3c3b3a53ba30db9f77f5b3bcf2d5e58ed9c0',
-    ),
-    'wp-cli/wp-config-transformer' => 
-    array (
-      'pretty_version' => 'v1.2.8',
-      'version' => '1.2.8.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '0bb2b9162c38ca72370380aea11dc06e431e13a5',
-    ),
-  ),
 );
